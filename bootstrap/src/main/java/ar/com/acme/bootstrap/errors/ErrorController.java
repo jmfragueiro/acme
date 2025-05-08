9d3fe897db942314d139769040357bbd6aef9964
jmfragueiro <jmfragueiro@hotmail.com>
2025-05-08 12:15:10 -0300

    avances

--- bootstrap/src/main/java/ar/com/acme/bootstrap/errors/ErrorController.java
+++ bootstrap/src/main/java/ar/com/acme/bootstrap/errors/ErrorController.java
@@ -11,9 +11,6 @@ import ar.com.acme.commons.Constants;
 import ar.com.acme.commons.Tools;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -24,29 +21,9 @@ public class ErrorController {
     @GetMapping
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public Map<String, String> errorGet(HttpServletRequest request, HttpServletResponse response) {
-        return this.getErroMap(request.getAttribute(errMsgAttr).toString());
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public Map<String, String> errorPost(HttpServletRequest request, HttpServletResponse response) {
-        return this.getErroMap(request.getAttribute(errMsgAttr).toString());
-    }
-
-    @PutMapping
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public Map<String, String> errorPut(HttpServletRequest request, HttpServletResponse response) {
-        return this.getErroMap(request.getAttribute(errMsgAttr).toString());
-    }
-
-    @DeleteMapping
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public Map<String, String> errorDelete(HttpServletRequest request, HttpServletResponse response) {
-        return this.getErroMap(request.getAttribute(errMsgAttr).toString());
-    }
-
-    private Map<String, String> getErroMap(String error) {
         return Map.of(Constants.SYS_CAD_RESPONSE_ERROR_MSG,
-                      Tools.getCadenaErrorFormateada(Constants.MSJ_USR_ERR_UNPROCESSABLE, error,null));
+                      Tools.getCadenaErrorFormateada(Constants.MSJ_USR_ERR_UNPROCESSABLE,
+                                                     request.getAttribute(errMsgAttr).toString(),
+                                                     null));
     }
 }
